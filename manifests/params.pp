da37b1675a0ab02816638776fe42715486fdf9e6
Lee Briggs <lee@leebriggs.co.uk>
2016-05-25 15:36:03 +0100

    Bump version of teleport

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,7 @@
 # It sets variables according to platform
 class teleport::params {
   
-  $version         = 'v0.1.0-alpha.7'
+  $version         = 'v0.2.0-beta.1'
   $archive_path    = '/tmp/teleport.tar.gz'
   $extract_path    = "/opt/teleport-${version}"
   $bin_dir         = '/usr/local/bin'
